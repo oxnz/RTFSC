0e94c008987bfc1c5398e9468a6662b27e02f70b
oxnz <yunxinyi@gmail.com>
2015-10-02 11:16:50 +0800

    add inet_{ntop,pton} demos

--- net/broadcast/bug.c
+++ net/broadcast/bug.c
@@ -1,12 +1,27 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <sys/types.h>
+#include <err.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <assert.h>
 
 int main(void) {
-	in_addr_t addr = inet_addr("255.255.255.255");
-	assert(addr == -1);
+	const char *broadcast_addr = "255.255.255.255";
+	in_addr_t addr;
+	
+	assert(inet_addr(broadcast_addr) == -1);
+
+	char buf[INET_ADDRSTRLEN];
+
+	if (inet_pton(AF_INET, broadcast_addr, &addr) <= 0)
+		err(1, "inet_pton");
+	assert(addr == addr);
+	if (inet_ntop(AF_INET, &addr, buf, INET_ADDRSTRLEN) == NULL)
+		err(1, "inet_ntop");
+	assert(strcmp(buf, broadcast_addr) == 0);
+
 	exit(0);
 }
