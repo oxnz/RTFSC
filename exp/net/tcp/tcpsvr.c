1d41f8b42bea4314f2705bb9fb5bd1c83a63f342
pslzym <1176993703@qq.com>
2016-11-16 17:55:44 +0800

    update tcpsvr.c

--- exp/net/tcp/tcpsvr.c
+++ exp/net/tcp/tcpsvr.c
@@ -1,80 +1,80 @@
 #include <stdio.h>
-#include <sys/socket.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
-#include <unistd.h>
+#include <arpa/inet.h>
 
 #include "../service.h"
 
-int BUFFER_SIZE = 1024;
-
-void process_request(int client_sk, struct sockaddr_in client_addr)
+void process_request(int client_fd, struct sockaddr_in client_addr)
 {
-    char recv_buffer[BUFFER_SIZE], send_buffer[BUFFER_SIZE];
-    while(1)
+    char recv_buffer[REQMAXLEN + 1], send_buffer[REQMAXLEN];
+    memset(recv_buffer, '\0', sizeof(recv_buffer));
+    int len = read(client_fd, recv_buffer, sizeof(recv_buffer));
+    printf("recv data: %s.\nrecv len : %d\n", recv_buffer, len);
+    memset(send_buffer, 0, sizeof(send_buffer));
+    if (strcmp(recv_buffer, "login") == 0)
     {
-        memset(recv_buffer, '\0', sizeof(recv_buffer));
-        int len = recv(client_sk, recv_buffer, sizeof(recv_buffer), 0);
-        printf("recv data: %s.\nrecv len : %d\n", recv_buffer, len);
-        if (strcmp(recv_buffer, "login") == 0)
-        {
-            memset(send_buffer, 0, sizeof(send_buffer));
-            strcpy(send_buffer, "welcome");
-            send(client_sk, send_buffer, sizeof(send_buffer), 0);
-        }
-        if (strcmp(recv_buffer, "logout") == 0)
-        {
-            memset(send_buffer, 0, sizeof(send_buffer));
-            strcpy(send_buffer, "bye");
-            send(client_sk, send_buffer, sizeof(send_buffer), 0);
-        }
-        break;
+        strcpy(send_buffer, "welcome");
+    }
+    else if (strcmp(recv_buffer, "logout") == 0)
+    {
+        strcpy(send_buffer, "bye");
+    }
+    else
+    {
+        strcpy(send_buffer, "error");
+    }
+    int wnum = write(client_fd, send_buffer, sizeof(send_buffer));
+    if (wnum != sizeof(send_buffer))
+    {
+        perror("write");
+        exit(-1);
     }
 }
 
 int main()
 {
-    struct sockaddr_in serv;
-    int socketfd, client_sk;
-    struct sockaddr_in client_addr;
+    struct sockaddr_in serv_addr, client_addr;
+    int serv_fd, client_fd;
     socklen_t len = sizeof(client_addr);
 
-    if ((socketfd = socket(PF_INET, SOCK_STREAM, 0)) < 0)
+    if ((serv_fd = socket(PF_INET, SOCK_STREAM, 0)) < 0)
     {
-        printf("socket error.\n");
+        perror("socket");
         return -1;
     }
-    printf("socket connect.\n");
-    memset(&serv, 0, sizeof(serv));
-    serv.sin_family = AF_INET;
-    serv.sin_addr.s_addr = htonl(INADDR_ANY);
-    serv.sin_port = htons(TCP_SRV_PORT);
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_addr.s_addr = inet_addr(TCP_SRV_ADDR);
+    serv_addr.sin_port = htons(TCP_SRV_PORT);
 
-    if (bind(socketfd, (struct sockaddr *)&serv, sizeof(serv)) < 0)
+    if (bind(serv_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     {
-        printf("bind error.\n");
-        return -2;
+        perror("bind");
+        return -1;
     }
     printf("bind success.\n");
 
-    if (listen(socketfd, REQMAXLEN) < 0)
+    if (listen(serv_fd, 20) < 0)
     {
-        printf("listen error.\n");
-        return -3;
+        perror("listen");
+        return -1;
     }
     printf("listen success.\n");
 
     int flag = 1;
     while(flag)
     {
-        if ((client_sk = accept(socketfd, (struct sockaddr *) &client_addr, &len)) < 0)
+        if ((client_fd = accept(serv_fd, (struct sockaddr *) &client_addr, &len)) < 0)
         {
-            printf("accept error.");
-            return -4;
+            perror("accept");
+            return -1;
         }
-        process_request(client_sk, client_addr);
-        close(client_sk);
+        process_request(client_fd, client_addr);
+        close(client_fd);
     }
-    return 1;
+    return 0;
 }
