64b36caf0b36eb6c8305bd3813cf2f24e0301d5a
云心逸 <yunxinyi+github@gmail.com>
2016-11-16 14:30:37 +0800

    add service.h, refine readme

---
+++ exp/net/service.h
@@ -0,0 +1,32 @@
+/*
+ * Filename:	common.h
+ *
+ * Author:		Oxnz
+ * Email:		yunxinyi@gmail.com
+ * Created:		[2016-11-16 14:25:01 CST]
+ * Last-update:	2016-11-16 14:25:01 CST
+ * Description: anchor
+ *
+ * Version:		0.0.1
+ * Revision:	[NONE]
+ * Revision history:	[NONE]
+ * Date Author Remarks:	[NONE]
+ *
+ * License: 
+ * Copyright (c) 2013 Oxnz
+ *
+ * Distributed under terms of the [LICENSE] license.
+ * [license]
+ *
+ */
+
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+#define TCP_SRV_PORT 8000U
+#define TCP_SRV_ADDR "127.0.0.1"
+
+#define REQMAXLEN 1024L
+#define RESMAXLEN 1024L
+
+#endif//_COMMON_H_
