59b133bfbaefb0eadf77a82858ec144035dee867
max.zhang <yunxinyi+github@gmail.com>
2025-09-24 16:07:52 +0800

    update mosaic

--- mosaic/README.md
+++ mosaic/README.md
@@ -4,3 +4,9 @@ a tool leverage http range request to download file using multi-connection concu
 
 Mosaic: Suggests creating a complete picture from many small, individual tiles, just as the tool builds a complete file from many small data ranges.
 
+## Usage
+
+```shell
+mosaic https://proof.ovh.net/files/100Mb.dat
+mosaic https://old-releases.ubuntu.com/releases/8.04.0/ubuntu-8.04.4-desktop-amd64.iso
+```
--- mosaic/src/main.rs
+++ mosaic/src/main.rs
@@ -1,108 +1,133 @@
-use std::ffi::c_void;
+use std::{
+    ffi::{CStr, CString, c_void},
+    path::PathBuf,
+    process::ExitCode,
+    str::FromStr,
+};
 
 use futures::{StreamExt, TryFutureExt, stream};
+use reqwest::Url;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), ExitCode> {
+    let argv = std::env::args().collect::<Vec<_>>();
+    let (uri, path) = match argv.len() {
+        2 => {
+            let uri = Url::try_from(argv[1].as_str()).expect("invalid uri");
+            let path = PathBuf::try_from(uri.path()).expect("invalid path");
+            let name = path
+                .file_name()
+                .and_then(|s| s.to_str())
+                .expect("missing filename");
+            (
+                uri.to_string(),
+                CString::from_str(name).expect("invalid path"),
+            )
+        }
+        3 => (
+            argv[1].to_string(),
+            CString::from_str(&argv[2]).expect("invalid path"),
+        ),
+        _ => {
+            eprintln!("usage: {} <uri> [pathname]", argv[0]);
+            return Err(ExitCode::FAILURE);
+        }
+    };
+
     tracing_subscriber::fmt()
         .with_env_filter(
             tracing_subscriber::EnvFilter::from_default_env()
                 .add_directive(tracing::Level::INFO.into()),
         )
         .init();
-    let uri = "https://proof.ovh.net/files/100Mb.dat";
-    let uri = "https://old-releases.ubuntu.com/releases/8.04.0/ubuntu-8.04.4-desktop-amd64.iso";
-    let retry_policy = reqwest::retry::for_host(uri)
-        .classify_fn(|req_rep| {
-            tracing::info!("retryable: {:?}", req_rep);
-            match req_rep.status() {
-                Some(reqwest::StatusCode::REQUEST_TIMEOUT) => req_rep.retryable(),
-                // _ => req_rep.success()
-                _ => req_rep.retryable(),
-            }
-        })
-        .no_budget();
-    let client = reqwest::ClientBuilder::new()
-        .retry(retry_policy)
-        .build()
-        .unwrap();
-    let resp = client.head(uri).send().await.unwrap();
+
+    let client = reqwest::Client::new();
+    let resp = client.head(&uri).send().await.unwrap();
     let headers = resp.headers();
     headers
         .get(reqwest::header::ACCEPT_RANGES)
         .expect("do not support range");
-    let len: usize = resp
+    let size: usize = resp
         .headers()
         .get(reqwest::header::CONTENT_LENGTH)
-        .expect("no content length")
+        .expect("missing content-length")
         .to_str()
         .expect("invalid value")
         .parse()
         .unwrap();
-    tracing::info!("content-length: {len}");
+    tracing::info!("content-length: {size}");
     let fd = unsafe {
         let fd = libc::open(
-            "mosaic/f.tmp\0".as_ptr(),
+            path.as_ptr(),
             libc::O_CREAT | libc::O_WRONLY,
             libc::S_IRUSR | libc::S_IWUSR | libc::S_IRGRP,
         );
         if fd < 0 {
             tracing::error!("failed to open");
-            return;
+            return Err(ExitCode::FAILURE);
         }
-        if libc::fallocate(fd, 0, 0, len as i64) < 0 {
+        if libc::fallocate(fd, 0, 0, size as i64) < 0 {
             tracing::error!("fallocate");
-            return;
+            return Err(ExitCode::FAILURE);
         }
         fd
     };
     let chunk_size = 4 * 1024 * 1024;
-    stream::iter(
-        (0..len)
-            .step_by(chunk_size)
-            .map(|offset| (offset, (offset + chunk_size - 1).min(len - 1))),
-    )
-    .map(|(offset, end)| (offset, fetch(&client, uri, offset, end)))
+    stream::iter((0..size).step_by(chunk_size).map(|offset| {
+        (
+            offset,
+            if offset + chunk_size < size {
+                chunk_size
+            } else {
+                size % chunk_size
+            },
+        )
+    }))
+    .map(|(offset, len)| (offset, fetch(&client, &uri, offset, len)))
     .for_each_concurrent(32, |(offset, fut)| async move {
         match fut.await {
             Ok(content) => {
-                let n = unsafe {
+                if unsafe {
                     libc::pwrite(
                         fd,
                         content.as_ptr() as *const c_void,
                         content.len(),
                         offset as i64,
                     )
-                };
-                if n < 0 {
+                } != content.len() as isize
+                {
                     tracing::error!("write error");
                 }
                 tracing::info!("done for offset: {offset}, len: {}", content.len());
             }
             Err(e) => {
-                tracing::error!("download failed for offset: {offset}, error: {e:?}");
+                tracing::error!("failed to fetch chunk with offset: {offset}, error: {e:?}");
             }
         }
     })
     .await;
-    // libc::fallocate(fd, mode, offset, len)
+
     tracing::info!("Hello, world!");
+
+    Ok(())
 }
 
 async fn fetch(
     client: &reqwest::Client,
     uri: &str,
     start: usize,
-    end: usize,
-) -> Result<Vec<u8>, reqwest::Error> {
+    len: usize,
+) -> reqwest::Result<bytes::Bytes> {
     loop {
         match client
             .get(uri)
-            .header(reqwest::header::RANGE, format!("bytes={}-{}", start, end))
+            .header(
+                reqwest::header::RANGE,
+                format!("bytes={}-{}", start, start + len),
+            )
             .send()
             .and_then(|resp| async move { resp.bytes().await })
             .await
-            .map(|bytes| bytes.to_vec())
         {
             Ok(content) => return Ok(content),
             Err(e) => {
