1110f1713f05a06a17fe85b32932e5de393a4f4c
Max Zhang <max.zhang@airwallex.com>
2025-10-06 22:33:48 +0800

    skip /dev/fd for ttyname scan

--- examples/fig_18_15.rs
+++ examples/fig_18_15.rs
@@ -15,32 +15,33 @@ fn ttyname(fd: libc::c_int) -> std::io::Result<String> {
     };
     let mut paths: Vec<PathBuf> = vec![PathBuf::from("/dev")];
     while let Some(path) = paths.pop() {
-        if let Ok(dir) = std::fs::read_dir(path) {
-            for ent in dir {
-                let p = ent?.path();
-                let name = p.to_str().unwrap();
-                let metadata = std::fs::metadata(&p).unwrap();
-                if metadata.is_dir() {
-                    paths.push(p);
+        for ent in std::fs::read_dir(path)? {
+            let p = ent?.path();
+            let name = p.to_str().unwrap();
+            let metadata = std::fs::metadata(&p).unwrap();
+            if metadata.is_dir() {
+                if name == "/dev/fd" {
                     continue;
                 }
-                let stat = {
-                    let c_path = std::ffi::CString::new(p.as_os_str().as_bytes()).expect("cstr");
-                    let mut stat = MaybeUninit::<libc::stat>::uninit();
-                    if unsafe { libc::stat(c_path.as_ptr(), stat.as_mut_ptr()) } < 0 {
-                        eprintln!("error");
-                        continue;
-                    }
-                    unsafe { stat.assume_init() }
-                };
-                match name {
-                    "/dev/stdin" | "/dev/stdout" | "/dev/stderr" => {
-                        // skip aliases
-                    }
-                    _ => {
-                        if stat.st_ino == fstat.st_ino && stat.st_dev == fstat.st_dev {
-                            return Ok(name.to_string());
-                        }
+                paths.push(p);
+                continue;
+            }
+            let stat = {
+                let c_path = std::ffi::CString::new(p.as_os_str().as_bytes()).expect("cstr");
+                let mut stat = MaybeUninit::<libc::stat>::uninit();
+                if unsafe { libc::stat(c_path.as_ptr(), stat.as_mut_ptr()) } < 0 {
+                    eprintln!("error");
+                    continue;
+                }
+                unsafe { stat.assume_init() }
+            };
+            match name {
+                "/dev/stdin" | "/dev/stdout" | "/dev/stderr" => {
+                    // skip aliases
+                }
+                _ => {
+                    if stat.st_ino == fstat.st_ino && stat.st_dev == fstat.st_dev {
+                        return Ok(name.to_string());
                     }
                 }
             }
