c26ac3a1f92cfa95c9450de175a2d7d5cba72d9d
Max Zhang <max.zhang@airwallex.com>
2025-10-14 14:17:49 +0800

    mmap

--- examples/fig_14_27.rs
+++ examples/fig_14_27.rs
@@ -1,5 +1,7 @@
 use std::{os::fd::AsRawFd, process::ExitCode};
 
+use rtfsc::{mmap, munmap};
+
 fn main() -> Result<(), ExitCode> {
     let argv = std::env::args().collect::<Vec<_>>();
     if argv.len() != 3 {
@@ -27,39 +29,29 @@ fn main() -> Result<(), ExitCode> {
     let chunk_size = 1 << 30; // 1GB
     let total_len = len;
     while offset < total_len {
-        let len =  chunk_size.min(total_len - offset) as usize;
-        let src = unsafe {
-            libc::mmap(
-                std::ptr::null_mut(),
-                len,
-                libc::PROT_READ,
-                libc::MAP_SHARED,
-                fd_in,
-                offset as i64,
-            )
-        };
-        if src == libc::MAP_FAILED {
-            unsafe { libc::perror("mmap".as_ptr() as *const _) };
-            return Err(ExitCode::FAILURE);
-        }
-        let dst = unsafe {
-            libc::mmap(
-                std::ptr::null_mut(),
-                len,
-                libc::PROT_WRITE,
-                libc::MAP_SHARED,
-                fd_out,
-                offset as i64,
-            )
-        };
-        if src == libc::MAP_FAILED {
-            unsafe { libc::perror("mmap".as_ptr() as *const _) };
-            return Err(ExitCode::FAILURE);
-        }
+        let len = chunk_size.min(total_len - offset) as usize;
+        let src = mmap(
+            std::ptr::null_mut(),
+            len,
+            libc::PROT_READ,
+            libc::MAP_SHARED,
+            fd_in,
+            offset as i64,
+        )
+        .unwrap();
+        let dst = mmap(
+            std::ptr::null_mut(),
+            len,
+            libc::PROT_WRITE,
+            libc::MAP_SHARED,
+            fd_out,
+            offset as i64,
+        )
+        .unwrap();
 
-        unsafe { libc::memcpy(dst, src, len) };
-        unsafe { libc::munmap(src, len) };
-        unsafe { libc::munmap(dst, len) };
+        unsafe { std::ptr::copy_nonoverlapping(src, dst, len) };
+        munmap(src, len).unwrap();
+        munmap(dst, len).unwrap();
         offset += len as u64;
     }
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,30 @@
+use std::ffi::c_void;
+
 pub mod file_op;
 pub mod flock;
 pub mod signal;
+
+
+pub fn mmap(
+    addr: *mut c_void,
+    len: usize,
+    prot: i32,
+    flags: i32,
+    fd: i32,
+    offset: i64,
+) -> std::io::Result<*mut c_void> {
+    let r = unsafe { libc::mmap(addr, len, prot, flags, fd, offset) };
+    if libc::MAP_FAILED == r {
+        Err(std::io::Error::last_os_error())
+    } else {
+        Ok(r)
+    }
+}
+
+pub fn munmap(addr: *mut c_void, len: usize) -> std::io::Result<()> {
+    if -1 == unsafe { libc::munmap(addr, len) } {
+        Err(std::io::Error::last_os_error())
+    } else {
+        Ok(())
+    }
+}
