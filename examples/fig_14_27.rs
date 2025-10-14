0136b7a03f52bc06a9a897cdeae991499c44393c
Max Zhang <max.zhang@airwallex.com>
2025-10-14 14:07:00 +0800

    Update fig_14_27.rs

--- examples/fig_14_27.rs
+++ examples/fig_14_27.rs
@@ -1,6 +1,4 @@
-use std::{mem::MaybeUninit, process::ExitCode};
-
-use libc::{MAP_SHARED, c_char};
+use std::{os::fd::AsRawFd, process::ExitCode};
 
 fn main() -> Result<(), ExitCode> {
     let argv = std::env::args().collect::<Vec<_>>();
@@ -9,56 +7,35 @@ fn main() -> Result<(), ExitCode> {
         return Err(ExitCode::FAILURE);
     }
 
-    let fd_in = unsafe { libc::open(argv[1].as_ptr() as *const c_char, libc::O_RDONLY) };
-    if fd_in < 0 {
-        unsafe { libc::perror("open".as_ptr() as *const c_char) };
-        return Err(ExitCode::FAILURE);
-    }
+    let fin = std::fs::OpenOptions::new()
+        .read(true)
+        .open(&argv[1])
+        .unwrap();
+    let len = fin.metadata().unwrap().len();
+    let fd_in = fin.as_raw_fd();
+    let fout = std::fs::OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .open(&argv[2])
+        .unwrap();
+    fout.set_len(len).unwrap();
+    let fd_out = fout.as_raw_fd();
 
-    let fd_out = unsafe {
-        libc::open(
-            argv[2].as_ptr() as *const c_char,
-            libc::O_RDWR | libc::O_CREAT | libc::O_TRUNC,
-            (libc::S_IRUSR | libc::S_IWUSR | libc::S_IRGRP | libc::S_IROTH) as libc::c_uint,
-        )
-    };
-
-    if fd_in < 0 {
-        unsafe { libc::perror("open".as_ptr() as *const c_char) };
-        return Err(ExitCode::FAILURE);
-    }
-
-    let stat = unsafe {
-        let mut stat: MaybeUninit<libc::stat> = MaybeUninit::uninit();
-        if libc::fstat(fd_in, stat.as_mut_ptr()) < 0 {
-            libc::perror("fstat".as_ptr() as *const _);
-            return Err(ExitCode::FAILURE);
-        }
-        stat.assume_init()
-    };
-
-    if unsafe { libc::ftruncate(fd_out, stat.st_size) } < 0 {
-        unsafe { libc::perror("ftruncate".as_ptr() as *const _) };
-        return Err(ExitCode::FAILURE);
-    }
-
-    let mut offset = 0;
+    let mut offset: u64 = 0;
     let chunk_size = 1 << 30; // 1GB
-    while offset < stat.st_size {
-        let len = if stat.st_size - offset > chunk_size {
-            chunk_size
-        } else {
-            stat.st_size - offset
-        } as usize;
-
+    let total_len = len;
+    while offset < total_len {
+        let len =  chunk_size.min(total_len - offset) as usize;
         let src = unsafe {
             libc::mmap(
                 std::ptr::null_mut(),
                 len,
                 libc::PROT_READ,
-                MAP_SHARED,
+                libc::MAP_SHARED,
                 fd_in,
-                offset,
+                offset as i64,
             )
         };
         if src == libc::MAP_FAILED {
@@ -72,7 +49,7 @@ fn main() -> Result<(), ExitCode> {
                 libc::PROT_WRITE,
                 libc::MAP_SHARED,
                 fd_out,
-                offset,
+                offset as i64,
             )
         };
         if src == libc::MAP_FAILED {
@@ -83,7 +60,7 @@ fn main() -> Result<(), ExitCode> {
         unsafe { libc::memcpy(dst, src, len) };
         unsafe { libc::munmap(src, len) };
         unsafe { libc::munmap(dst, len) };
-        offset += len as i64;
+        offset += len as u64;
     }
 
     Ok(())
