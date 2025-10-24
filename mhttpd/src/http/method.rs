a929a948c8c6ef7550d703c4b55ad95eb84c0e49
Max Zhang <max.zhang@airwallex.com>
2025-10-24 14:40:59 +0800

    Update method.rs

--- mhttpd/src/http/method.rs
+++ mhttpd/src/http/method.rs
@@ -1,14 +1,16 @@
 use std::str::FromStr;
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum Method {
-    Head,
-    Options,
-    Get,
+    HEAD,
+    OPTIONS,
+    GET,
     POST,
     PUT,
     DELETE,
     CONNECT,
+    PATCH,
+    TRACE,
 }
 
 impl FromStr for Method {
@@ -16,14 +18,51 @@ impl FromStr for Method {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
-            "HEAD" => Ok(Self::Head),
-            "OPTIONS" => Ok(Self::Options),
-            "GET" => Ok(Self::Get),
+            "HEAD" => Ok(Self::HEAD),
+            "OPTIONS" => Ok(Self::OPTIONS),
+            "GET" => Ok(Self::GET),
             "POST" => Ok(Self::POST),
             "PUT" => Ok(Self::PUT),
             "DELETE" => Ok(Self::DELETE),
             "CONNECT" => Ok(Self::CONNECT),
-            _ => todo!(),
+            "PATCH" => Ok(Self::PATCH),
+            "TRACE" => Ok(Self::TRACE),
+            _ => Err(std::io::Error::new(
+                std::io::ErrorKind::InvalidInput,
+                "Invalid HTTP method",
+            )),
         }
+    }
+}
+
+impl std::fmt::Display for Method {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Method::HEAD => write!(f, "HEAD"),
+            Method::OPTIONS => write!(f, "OPTIONS"),
+            Method::GET => write!(f, "GET"),
+            Method::POST => write!(f, "POST"),
+            Method::PUT => write!(f, "PUT"),
+            Method::DELETE => write!(f, "DELETE"),
+            Method::CONNECT => write!(f, "CONNECT"),
+            Method::PATCH => write!(f, "PATCH"),
+            Method::TRACE => write!(f, "TRACE"),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse() {
+        assert!(matches!("HEAD".parse(), Ok(Method::HEAD)));
+        assert!(matches!("HEADx".parse::<Method>(), Err(_)));
+    }
+
+    #[test]
+    fn test_display() {
+        assert_eq!(Method::GET.to_string(), "GET");
     }
 }
