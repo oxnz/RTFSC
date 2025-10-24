e1cac77117f08617519f14de0ed39d5bccd21e8b
Max Zhang <max.zhang@airwallex.com>
2025-10-24 14:32:06 +0800

    Update scheme.rs

--- mhttpd/src/http/scheme.rs
+++ mhttpd/src/http/scheme.rs
@@ -1,4 +1,7 @@
-use std::str::FromStr;
+use std::{
+    fmt::{Display, write},
+    str::FromStr,
+};
 
 #[derive(Debug)]
 pub enum Scheme {
@@ -7,14 +10,22 @@ pub enum Scheme {
     Custom(String),
 }
 
-impl FromStr for Scheme {
-    type Err = ();
+impl From<&str> for Scheme {
+    fn from(value: &str) -> Self {
+        match value {
+            "http" => Self::Http,
+            "https" => Self::Https,
+            _ => Self::Custom(value.to_string()),
+        }
+    }
+}
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "http" => Ok(Self::Http),
-            "https" => Ok(Self::Https),
-            _ => Ok(Self::Custom(s.to_string())),
+impl Display for Scheme {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Scheme::Http => write!(f, "http"),
+            Scheme::Https => write!(f, "https"),
+            Scheme::Custom(value) => write!(f, "{value}"),
         }
     }
 }
@@ -25,8 +36,15 @@ mod tests {
 
     #[test]
     fn test_parse() {
-        assert!(matches!("http".parse(), Ok(Scheme::Http)));
-        assert!(matches!("https".parse(), Ok(Scheme::Https)));
-        assert!(matches!("httpx".parse(), Ok(Scheme::Custom(s)) if s == "httpx"));
+        assert!(matches!("http".into(), Scheme::Http));
+        assert!(matches!("https".into(), Scheme::Https));
+        assert!(matches!("httpx".into(), Scheme::Custom(s) if s == "httpx"));
+    }
+
+    #[test]
+    fn test_display() {
+        assert_eq!(Scheme::Http.to_string(), "http");
+        assert_eq!(Scheme::Https.to_string(), "https");
+        assert_eq!(Scheme::Custom("httpx".to_string()).to_string(), "httpx");
     }
 }
