5004fb834d86e41fff8c105d15e0457ed96f2f3b
Max Zhang <max.zhang@airwallex.com>
2025-10-26 15:21:00 +0800

    crate pub

--- mhttpd/src/http/codec/mod.rs
+++ mhttpd/src/http/codec/mod.rs
@@ -1,11 +1,18 @@
 use std::io::{BufRead, Write};
 
-pub trait Encode<Item> {
+pub(crate) trait Encode<Item> {
     fn encode<W: Write>(&mut self, item: Item, stream: &mut W) -> std::io::Result<()>;
 }
 
-pub trait Decode<Item> {
+pub(crate) trait Decode<Item> {
     fn decode<R: BufRead>(&mut self, stream: &mut R) -> std::io::Result<Item>;
 }
 
-pub trait Codec<Item>: Encode<Item> + Decode<Item> {}
+pub(crate) trait Codec<Item>: Encode<Item> + Decode<Item> {}
+
+pub(crate) trait SerDe {
+    fn read<R: BufRead>(r: &mut R) -> std::io::Result<Self>
+    where
+        Self: Sized;
+    fn write<W: Write>(&self, w: &mut W) -> std::io::Result<()>;
+}
--- mhttpd/src/http/message/mod.rs
+++ mhttpd/src/http/message/mod.rs
@@ -2,12 +2,3 @@ mod request;
 mod response;
 pub use request::{Request, RequestBuilder};
 pub use response::{Response, ResponseBuilder};
-
-use std::io::{BufRead, Write};
-
-pub trait SerDe {
-    fn read<R: BufRead>(r: &mut R) -> std::io::Result<Self>
-    where
-        Self: Sized;
-    fn write<W: Write>(&self, w: &mut W) -> std::io::Result<()>;
-}
