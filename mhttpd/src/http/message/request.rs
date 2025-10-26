5da54c6227e3c2f1a519b1e9b7aec1f11b850455
Max Zhang <max.zhang@airwallex.com>
2025-10-26 14:50:23 +0800

    v1 serde

--- mhttpd/src/http/message/request.rs
+++ mhttpd/src/http/message/request.rs
@@ -77,11 +77,11 @@ impl RequestBuilder {
 
 #[derive(Debug)]
 pub struct Request {
-    method: Method,
-    path: String,
-    version: Version,
-    headers: Vec<Header>,
-    body: Option<Vec<u8>>,
+    pub(crate) method: Method,
+    pub(crate) path: String,
+    pub(crate) version: Version,
+    pub(crate) headers: Vec<Header>,
+    pub(crate) body: Option<Vec<u8>>,
 }
 
 impl Request {
@@ -99,91 +99,5 @@ impl Request {
             headers,
             body,
         }
-    }
-}
-
-impl SerDe for Request {
-    fn read<R: BufRead>(r: &mut R) -> std::io::Result<Self>
-    where
-        Self: Sized,
-    {
-        let mut line = Vec::with_capacity(8 * 1024);
-        let n = r.read_until(b'\n', &mut line)?;
-        if n == 0 {
-            return Err(std::io::Error::new(
-                std::io::ErrorKind::WouldBlock,
-                "EAGAIN",
-            ));
-        }
-        let request_line = str::from_utf8(line.trim_ascii_end())
-            .map_err(|e| std::io::Error::new(std::io::ErrorKind::InvalidData, e))?;
-        let (method, target, protocol) = match request_line.split_once(' ') {
-            Some((method, rest)) => match rest.rsplit_once(' ') {
-                Some((target, protocol)) => (
-                    method.parse::<Method>()?,
-                    target.to_string(),
-                    protocol.parse::<Version>()?,
-                ),
-                None => todo!(),
-            },
-            None => {
-                return Err(std::io::Error::new(
-                    std::io::ErrorKind::InvalidInput,
-                    "invalid request line",
-                ));
-            }
-        };
-        let mut headers = Vec::new();
-        let mut content_length = 0;
-        loop {
-            line.clear();
-            r.read_until(b'\n', &mut line)?;
-            let s = str::from_utf8(&line.trim_ascii_end())
-                .map_err(|e| std::io::Error::new(std::io::ErrorKind::InvalidData, e))?;
-            if s.is_empty() {
-                break;
-            }
-            let header: Header = s.parse()?;
-            if let Header::Literal { name: _, value } = &header {
-                content_length = value.parse().unwrap();
-            }
-
-            headers.push(header);
-        }
-        let body = if content_length != 0 {
-            let mut content = vec![0; content_length];
-            r.read_exact(&mut content)?;
-            Some(content)
-        } else {
-            None
-        };
-        Ok(Self {
-            method,
-            path: target,
-            version: protocol,
-            headers,
-            body,
-        })
-    }
-
-    fn write<W: Write>(&self, w: &mut W) -> std::io::Result<()> {
-        todo!()
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::http::SerDe;
-
-    use super::*;
-
-    #[test]
-    fn test_request_read() {
-        let input = b"POST /submit HTTP/1.1\r\nHost: example.com\r\nContent-Type: application/x-www-form-urlencoded\r\nContent-Length: 14\r\n\r\ncomment=hello!";
-        let request = Request::read(&mut std::io::Cursor::new(input)).unwrap();
-        println!("{request:?}");
-        assert_eq!(request.method, Method::POST);
-        assert_eq!(request.path, "/submit");
-        assert_eq!(request.headers.len(), 3);
     }
 }
--- mhttpd/src/http/v1/codec.rs
+++ mhttpd/src/http/v1/codec.rs
@@ -1,57 +1,108 @@
-use std::io::{BufWriter, Write};
+use std::io::{BufRead, Write};
 
-use crate::http::{
-    Request,
-    codec::{Decode, Encode},
-};
+use crate::http::{Header, Method, Request, SerDe, Version};
 
-#[derive(Debug)]
-pub struct RequestCodec {
-    max_len_limit: usize,
-}
-
-impl Default for RequestCodec {
-    fn default() -> Self {
-        Self {
-            max_len_limit: usize::MAX,
+impl SerDe for Request {
+    fn read<R: BufRead>(r: &mut R) -> std::io::Result<Self>
+    where
+        Self: Sized,
+    {
+        let mut line = Vec::with_capacity(8 * 1024);
+        let n = r.read_until(b'\n', &mut line)?;
+        if n == 0 {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::WouldBlock,
+                "EAGAIN",
+            ));
         }
-    }
-}
+        let request_line = str::from_utf8(line.trim_ascii_end())
+            .map_err(|e| std::io::Error::new(std::io::ErrorKind::InvalidData, e))?;
+        let (method, path, version) = match request_line.split_once(' ') {
+            Some((method, rest)) => match rest.rsplit_once(' ') {
+                Some((target, protocol)) => (
+                    method.parse::<Method>()?,
+                    target.to_string(),
+                    protocol.parse::<Version>()?,
+                ),
+                None => todo!(),
+            },
+            None => {
+                return Err(std::io::Error::new(
+                    std::io::ErrorKind::InvalidInput,
+                    "invalid request line",
+                ));
+            }
+        };
+        let mut headers = Vec::new();
+        let mut content_length = 0;
+        loop {
+            line.clear();
+            r.read_until(b'\n', &mut line)?;
+            let s = str::from_utf8(&line.trim_ascii_end())
+                .map_err(|e| std::io::Error::new(std::io::ErrorKind::InvalidData, e))?;
+            if s.is_empty() {
+                break;
+            }
+            let header: Header = s.parse()?;
+            if let Header::Literal { name, value } = &header {
+                if name == "content-length" {
+                    content_length = value
+                        .parse()
+                        .map_err(|e| std::io::Error::new(std::io::ErrorKind::InvalidData, e))?;
+                }
+            }
 
-impl Encode<Request> for RequestCodec {
-    fn encode<W: std::io::Write>(
-        &mut self,
-        request: Request,
-        stream: &mut W,
-    ) -> std::io::Result<()> {
-        let mut stream = BufWriter::new(stream);
-        // for line in item.as_ref().lines() {
-        //     let sep = b"\r\n";
-        //     let len = line.len() + sep.len();
-        //     if len <= self.max_len_limit {
-        //         stream.write_all(line.as_bytes())?;
-        //         stream.write_all(sep.as_slice())?;
-        //     } else {
-        //         return Err(std::io::Error::new(
-        //             std::io::ErrorKind::InvalidData,
-        //             "line too long",
-        //         ))
-        //     }
-        // }
-        stream.flush()
+            headers.push(header);
+        }
+        let body = if content_length != 0 {
+            let mut content = vec![0; content_length];
+            r.read_exact(&mut content)?;
+            Some(content)
+        } else {
+            None
+        };
+        Ok(Self {
+            method,
+            path,
+            version,
+            headers,
+            body,
+        })
+    }
+
+    fn write<W: Write>(&self, stream: &mut W) -> std::io::Result<()> {
+        write!(stream, "{} {} {}\r\n", self.method, self.path, self.version)?;
+        for header in &self.headers {
+            match header {
+                Header::Pseudo { name, value } | Header::Literal { name, value } => {
+                    write!(stream, "{}: {}\r\n", name, value)?;
+                }
+            }
+        }
+        stream.write_all(b"\r\n")?;
+        if let Some(content) = &self.body {
+            stream.write_all(&content)?;
+        }
+        Ok(())
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::http::{Method, SerDe};
+
     use super::*;
 
     #[test]
-    fn test_codec() {
-        let raw = "POST / HTTP/1.1\r\nContent-Type: text/csv\r\n\r\nabc,def\r\n";
-        let mut stream = Vec::new();
-        let mut codec = RequestCodec::default();
-
-        assert_eq!(raw.as_bytes(), stream);
+    fn test_request_read() {
+        let input = b"POST /submit HTTP/1.1\r\nHost: example.com\r\nContent-Type: application/x-www-form-urlencoded\r\nContent-Length: 14\r\n\r\ncomment=hello!";
+        let request = Request::read(&mut std::io::Cursor::new(input)).unwrap();
+        println!("{request:?}");
+        assert_eq!(request.method, Method::POST);
+        assert_eq!(request.path, "/submit");
+        assert_eq!(request.headers.len(), 3);
+        let mut buffer = Vec::new();
+        request.write(&mut buffer).unwrap();
+        assert_eq!(input.len(), buffer.len());
     }
 }
