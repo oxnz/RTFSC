35038ef2e3e07dab1f000cf5556766724f515005
Max Zhang <max.zhang@airwallex.com>
2025-10-26 16:04:14 +0800

    client init

---
+++ mhttpd/src/client/mod.rs
@@ -0,0 +1,12 @@
+use crate::http::{v2::connection::Connection, Response};
+
+#[derive(Debug)]
+pub struct Client {
+    connection: Connection,
+}
+
+impl Client {
+    pub async fn request() -> std::io::Result<Response> {
+        todo!()
+    }
+}
